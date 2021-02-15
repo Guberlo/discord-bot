71854ca1966de180184d083f6f3a60fffe8509cf
Guberlo <salasero24@gmail.com>
2021-02-15 13:32:15 +0100

    feat: add other apis

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ module.exports = {
     token:  process.env.TOKEN,
     prefix: process.env.PREFIX,
     spoon_api: process.env.SPOON_API_KEY,
-    nasa_api: process.env.NASA_API_KEY
+    nasa_api: process.env.NASA_API_KEY,
+    strain_api: process.env.STRAIN_API_KEY,
+    bing_api: process.env.BING_API_KEY
 };
