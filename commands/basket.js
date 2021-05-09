9353727c3521c9b07caf3d6ed3ae0bc8f0968ebb
Guberlo <salasero24@gmail.com>
2021-05-09 12:34:46 +0200

    chore: comments and minor changes

--- commands/basket.js
+++ commands/basket.js
@@ -11,22 +11,24 @@ module.exports = {
     execute(message, args) {
         message.channel.send(shootMessage)
             .then(msg => {
+                // After one second edit the message sent before and move the ball
                 setTimeout(() => {
                     msg.edit(secondMessage);
                 }, 1000);
+                // After two seconds edit the message and decide whether it's a score or not
                 setTimeout(() => {
                     if (Math.floor(Math.random() * 10) === 0)
-                    msg.edit(netMessage);
-                else {
-                    let spacesToRemove = -Math.floor(Math.random() * 20) + 23;
-                    let s = netMessage.split('\n');
-                    s[0] = s[0].replace('canestro: grodato', 'cacato nel puzzo');
-                    s[1] = s[1].slice(spacesToRemove);
+                        msg.edit(netMessage);
+                    else {
+                        // Random "distance" from the ball to the net
+                        let spacesToRemove = -Math.floor(Math.random() * 20) + 23;
 
-                    let res = s[0].concat('\n').concat(s[1]);
+                        let s = netMessage.split('\n');
+                        s[0] = s[0].replace('canestro: grodato', 'cacato nel puzzo');
+                        s[1] = s[1].slice(spacesToRemove);
 
-                    msg.edit(res);
-                }
+                        msg.edit(s[0].concat('\n').concat(s[1]));
+                    }
                 }, 2000);
             });
     }
--- commands/sleeper.js
+++ commands/sleeper.js
@@ -1,18 +1,33 @@
+const imagesNumber = 7;
+const min = 3;
+
 module.exports = {
     name: 'sleeper',
     description: 'cazzp svegliati dai',
     execute(message, args) {
-        if (Math.floor(Math.random() * 999) == 1)
+        if (message.author.tag === 'Bakarmelo#8560') {
+            if (Math.floor(Math.random() * 999) == 1)
+                message.channel.send({
+                    files: [{
+                        attachment: 'data/sleeper2.jpg'
+                    }]
+                });
+            else
+                message.channel.send({
+                    files: [{
+                        attachment: 'data/sleeper.jpg'
+                    }]
+                });
+        }
+        else {
+            // Number between min and max number 
+            let img = Math.floor(Math.random() * (imagesNumber - min + 1)) + min;
             message.channel.send({
                 files: [{
-                    attachment: 'data/sleeper2.jpg'
+                    attachment: `data/sleeper${img}.jpg`
                 }]
             });
-        else
-            message.channel.send({
-                files: [{
-                    attachment: 'data/sleeper.jpg'
-                }]
-            });
+        }
+
     }
 }
